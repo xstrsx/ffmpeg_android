e483487491abc56b1222590cdf1747bcc0e34c2b
xstrsx <1157656327@qq.com>
2026-05-30 15:37:56 +0800

    添加插件管理和依赖解析管理配置

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,3 +1,19 @@
+pluginManagement {
+    repositories {
+        google()
+        gradlePluginPortal()
+        mavenCentral()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
 rootProject.name = "ffmpeg-terminal-tool"
 
 include(":app")
